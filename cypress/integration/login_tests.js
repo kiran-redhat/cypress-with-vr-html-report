6406333f49988da862080d8bf79886f88dd7f0b3
Kiran <kiran@Kirans-MacBook-Pro.local>
2021-08-15 17:55:31 +1200

    Initial commit with login and register tests

---
+++ cypress/integration/login_tests.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('Login tests', function(){
+
+    it('Valid user can login successfully', function(){
+        cy.visit('/');
+        cy.get('.input-sm').type('kiran.redhat@gmail.com');
+        cy.get(".pull-xs-right .ng-pristine").type('MySuperSecreat@123');
+        cy.get('.btn-success').click();
+        cy.get('.pull-xs-right .nav-item').should('be.contain', 'Hi, Kiran');
+    });
+
+    it('In valid user can not login and shown with an error message', function(){
+        cy.visit('/');
+        cy.get('.input-sm').type('Non.existing.user@kiran.com');
+        cy.get(".pull-xs-right .ng-pristine").type('MySuperSecreat@123');
+        cy.get('.btn-success').click();
+        cy.get('.label-warning').should('be.contain', "Invalid username/password");
+    });
+})
