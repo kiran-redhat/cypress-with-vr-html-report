7ab923d8dc8171dffba91bb2d196d3d00edc9ce6
Kiran Chilakamarri <kiran.redhat@gmail.com>
2023-07-10 22:07:10 +1200

    Update cypress to support cy 12.x

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,24 @@
-const { defineConfig } = require("cypress");
-const { addMatchImageSnapshotPlugin } = require("@simonsmith/cypress-image-snapshot");
+const {defineConfig} = require('cypress');
+const {addMatchImageSnapshotPlugin} = require('@simonsmith/cypress-image-snapshot/plugin');
 
 module.exports = defineConfig({
     e2e: {
         projectId: "cypress-with-vr-html-report",
         baseUrl: "https://buggy.justtestit.org",
-        chromeWebSecurity: false,        
+        chromeWebSecurity: false,
+        reporter: "cypress-mochawesome-reporter",
+        reporterOptions: {
+            "reportDir": "cypress/reports",
+            "charts": true,
+            "reportPageTitle": "My Test Suite",
+            "embeddedScreenshots": true,
+            "inlineAssets": true
+          },
+        screenshotsFolder: "cypress/reports/screenshots",
+        video: false,
+        defaultCommandTimeout: 6000,                
         setupNodeEvents(on, config) {
-            require('cypress-mochawesome-reporter/plugin')(on);
             addMatchImageSnapshotPlugin(on, config);
         },
     },
-    reporter: "cypress-mochawesome-reporter",
-    reporterOptions: {
-        "reportDir": "cypress/reports",
-        "charts": true,
-        "reportPageTitle": "My Test Suite",
-        "embeddedScreenshots": true,
-        "inlineAssets": true
-      },
-    screenshotsFolder: "cypress/reports/screenshots",
-    video: false,
-    defaultCommandTimeout: 6000,
-})
-
-
+  })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,4 @@
-import { addMatchImageSnapshotCommand } from '@simonsmith/cypress-image-snapshot/command';
+import {addMatchImageSnapshotCommand} from '@simonsmith/cypress-image-snapshot/command'
 import './commands';
 
 /* Cypress while running visual automated tests thinks that there are no reference images
@@ -6,8 +6,11 @@ as screenshotsFolder is set to cypress/reports/screenshots
 and creating new snapshots under that folder and exiting without diffing. 
 By explicitly mentioning in customSnapshotDir, it allows cypress-image-snapshot plugin 
 just to run tests and create if there are any diffs */
-import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 addMatchImageSnapshotCommand({
+    failureThreshold: 0.00,
+    failureThresholdType: 'percent',
+    customDiffConfig: { threshold: 0.0 },
+    capture: 'viewport',    
     customSnapshotsDir: 'cypress/snapshots/',
     customDiffDir: 'cypress/reports/screenshots/'
 })
